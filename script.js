447baf8a147719995e39849c3cac388af998fc49
bakyt123 <bunckerjeik@gmail.com>
2022-12-14 16:55:58 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+﻿console.log(2);
+
+// alert('Who are you?');
+
+// const result = confirm('Are you here?');
+
+// console.log(result);
+
+
+// const answer = +prompt('Вам есть 18?', ''); // данные полученные от пользователя сттановится number
+
+// console.log(answer + 5);
+
+
+// const answer = prompt('Вам есть 18?', '');
+
+// console.log(typeof(answer));
+
+
+const answers = [];
+
+answers[0] = prompt('Введите ваше имя:');
+answers[1] = prompt('Введите вашу фамилию:');
+answers[2] = prompt('Введите ваш возраст:');
+
+document.write(answers);
